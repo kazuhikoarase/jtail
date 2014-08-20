893f85dda80daa8d023efc73158ec4292bf44e66
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2014-08-20 13:35:57 +0900

    Revert "refactor"
    This reverts commit e696baa9fcec6709e579e8935c517cd7e5a92ac2.

--- src/java/jtail/TailInput.java
+++ src/java/jtail/TailInput.java
@@ -19,79 +19,62 @@ public class TailInput {
 	private long waitIntervalInMillis;
 	private boolean alive;
 
-	private long start;
-	private ByteArrayOutputStream buf;
-
 	public TailInput(File file, long waitIntervalInMillis) {
 		this.file = file;
 		this.waitIntervalInMillis = waitIntervalInMillis;
 		this.alive = true;
 	}
 
-	public void shutdown() {
-		alive = false;
-	}
-
 	public void read(Consumer c) throws IOException {
 
-		start = file.length();
-		buf = null;
+		long start = file.length();
+		ByteArrayOutputStream buf = null;
 
 		while (alive) {
 
-			waitForInput();
-
-			if (!alive) {
-				return;
+			// wait for input
+			while (start == file.length() ) {
+				try {
+					Thread.sleep(waitIntervalInMillis);
+				} catch(InterruptedException e) {
+					throw new IOException(e);
+				}
+				if (!alive) {
+					return;
+				}
 			}
 
-			readFile(c);
-		}
-	}
+			if (start > file.length() ) {
+				// file changed.
+				start = 0;
+			}
 
-	protected void waitForInput() throws IOException {
-		while (alive && start == file.length() ) {
+			RandomAccessFile raf = new RandomAccessFile(file, "r");
 			try {
-				Thread.sleep(waitIntervalInMillis);
-			} catch(InterruptedException e) {
-				throw new IOException(e);
+
+				int b;
+
+				raf.seek(start);
+
+				while (alive && (b = raf.read() ) != -1) {
+					start += 1;
+					if (buf == null) {
+						buf = new ByteArrayOutputStream();
+					}
+					buf.write(b);
+					if (b == '\n') {
+						buf.close();
+						c.accept(buf.toByteArray() );
+						buf = null;
+					}
+				}
+			} finally {
+				raf.close();
 			}
 		}
 	}
-	
-	protected void readFile(Consumer c) throws IOException {
 
-		if (start > file.length() ) {
-			// file changed.
-			start = 0;
-		}
-
-		RandomAccessFile raf = new RandomAccessFile(file, "r");
-		try {
-			int b;
-			raf.seek(start);
-			while (alive && (b = raf.read() ) != -1) {
-				start += 1;
-				doByte(c, b);
-			}
-		} finally {
-			raf.close();
-		}
-	}
-
-	protected void doByte(Consumer c, int b) throws IOException {
-		if (buf == null) {
-			buf = new ByteArrayOutputStream();
-		}
-		buf.write(b);
-		if (b == '\n') {
-			buf.close();
-			doBytes(c, buf.toByteArray() );
-			buf = null;
-		}
-	}
-
-	protected void doBytes(Consumer c, byte[] bytes) throws IOException {
-		c.accept(bytes);
+	public void shutdown() {
+		alive = false;
 	}
 }
